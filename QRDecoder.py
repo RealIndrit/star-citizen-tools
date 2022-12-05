a2db942fb89c1f2768665400a460f45ce65fcc22
RealIndrit <johan.indrit.andersson@gmail.com>
2022-12-05 02:37:17 +0100

    Add info for QRDecoder

--- QRDecoder.py
+++ QRDecoder.py
@@ -37,13 +37,36 @@ def starcitizen(data):
             print(f'{qr_data} - {e}')
 
 
+def info():
+    return """
+   Session: Is your session id, it's bound to your current play-session, this session is created when the game is launched from your RSI launcher, and does not change if you switch server/regions in game, to change this sesion, you will have to relaunch the game
+
+   ShardId: The current server you are currently playing on, if this id is the same as your session, it means that no you were either in a loading screen or in a submenu of the main start menu
+
+   Time (Unix): Current unix time, seconds since UTC 1 January 1970, you can easily convert this to more readable date format on the internet
+
+   Build: Current active build being played
+
+   File: You know what this is :)
+
+   EXTRA: Source for this info is from leaked evocati gameplay with debug info active on the screen: https://streamable.com/0woa2q and https://cdn.discordapp.com/attachments/422137614854914048/1048887977025220619/Tavern-Upload-Image-473137-1670072591.png
+   """
+
+
 if __name__ == "__main__":
     parser = OptionParser(
         usage='%prog --target=test.png')
     parser.add_option('--target', dest='path',
                       help='target file with the path')
+    parser.add_option('--information', dest='info', action='store_true', default=False,
+                      help='Useful information on how to read the data')
     options, args = parser.parse_args()
     QR_data = []
+
+    if options.info:
+        print(info())
+        sys.exit(0)
+
     if not options.path:
         parser.print_help()
         sys.exit(0)
